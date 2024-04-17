5e580e1ac83e116eeff8fd8442828f66eca3d788
Vinicius Fillos <vinifillos@gmail.com>
2024-04-17 08:46:33 -0300

    Category

--- src/main/java/com/organization/course/config/TestConfig.java
+++ src/main/java/com/organization/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.organization.course.config;
 
+import com.organization.course.entities.Category;
 import com.organization.course.entities.User;
 import com.organization.course.entities.Order;
 import com.organization.course.entities.enums.OrderStatus;
+import com.organization.course.repositories.CategoryRepository;
 import com.organization.course.repositories.OrderRepository;
 import com.organization.course.repositories.UserRepository;
 import org.aspectj.weaver.ast.Or;
@@ -24,17 +26,26 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        userRepository.saveAll(Arrays.asList(u1,u2));
 
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
-
-        userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
     }
 }
---
+++ src/main/java/com/organization/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.organization.course.repositories;
+
+import com.organization.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
